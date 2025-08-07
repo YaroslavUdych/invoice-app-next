7ff6e0ad4ee4bd43b8cc97fb5548f11fba23e144
YaroslavUdych <slayes13@gmail.com>
2025-08-07 06:35:55 +0200

    chore(fonts): extend font weight property

--- src/fonts/league-spartan.ts
+++ src/fonts/league-spartan.ts
@@ -3,6 +3,6 @@ import { League_Spartan } from 'next/font/google';
 export const leagueSpartan = League_Spartan({
   subsets: ['latin'],
   variable: '--font-league-spartan',
-  weight: ['400', '500', '700'],
+  weight: ['400', '500', '600', '700'],
   display: 'swap',
 });
